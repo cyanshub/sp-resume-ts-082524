6a2a2c727a0710235f4f930622b012309ad45172
travel15740 <travel15740@gmail.com>
2024-08-24 15:09:20 +0800

    導入 bootstrap 框架樣式, 圖示資源及 sass 樣式架構

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ResumePage from './pages/ResumePage'
 import HomePage from './pages/HomePage'
 
+// 引入sass樣式架構
+import './App.scss'
+
+// 引入bootstrap框架樣式  
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+
 // 定義專案基礎路徑
 const basename = import.meta.env.VITE_PUBLIC_URL
 
