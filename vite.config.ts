e3ae0ae29231419885ded3b7f2527fed21cba3a4
travel15740 <travel15740@gmail.com>
2024-08-24 14:28:04 +0800

    進行專案環境設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
+import svgr from 'vite-plugin-svgr'
+import dotenv from 'dotenv'
+
+// 讀取 .env 文件
+dotenv.config()
+
+const baseURL = process.env.VITE_PUBLIC_URL
+console.log('應用程式基礎路徑:', baseURL)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  server: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  // base: 設定靜態檔案的放置路徑(通常是 GitHub 遠端儲存庫名稱)
+  base: baseURL
 })
